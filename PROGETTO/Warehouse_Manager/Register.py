e43f351a6cc6b228d9d5ff95c53393a14bd0bbb7
LiaCaterina <lia.caterina97@gmail.com>
2021-04-11 14:40:14 +0200

    AGGIORNAMENTO FORM
    aggiunta commenti agli script, cambiati i fields di thingspeak sulla form, aggiunta del file di "Setting" per prendere l'api key e l'url del catalog, cambiati i nomi dei file per rendere tutto più ordinato

--- PROGETTO/Warehouse_Manager/Register.py
+++ PROGETTO/Warehouse_Manager/Register.py
@@ -6,14 +6,14 @@ import requests
 class OpenWebService():
 	exposed = True
 	# l'url del catalog è noto a priori, unica cosa nota
-	def __init__(self):
-		self.url_catalog = "http://127.0.0.1:8070"
+	def __init__(self, url_catalog, api_key):
+		self.url_catalog = url_catalog
 		self.url_create_channel_thingspeak = "https://api.thingspeak.com/channels.json"
 		self.url_delete_channel_thingspeak = "https://api.thingspeak.com/channels" #+ channelID alla fine
-		self.api_key = "LL8A82J0EWRBDUSQ" #questo è l'api key di Caterina per creare massimo 4 canali
+		self.api_key = api_key #questo è l'api key di Caterina per creare massimo 4 canali
 
 	def GET(self, **params):
-		return open("prova3_tabs.html")
+		return open("myform.html")
 
 	def POST(self):
 		# qui bisogna inserire gli if che permettono di controllare 
@@ -36,8 +36,6 @@ class OpenWebService():
 		if new_device["command"] == "insert_room":
 			body = new_device["body"]
 			# seguenti righe per creare il canale ThingSpeak
-			#api_key=self.api_key&name=body["roonID"]
-			#body_new_channel = {"apy_key": self.api_key, "name": body["roomID"]}
 			print(self.url_create_channel_thingspeak + f'?api_key={self.api_key}&name={body["roomID"]}')
 			response = requests.post(self.url_create_channel_thingspeak + f'?api_key={self.api_key}&name={body["roomID"]}&field1=Temperature&field2=Humidity&field3=Smoke&field4=Empty&field5=Empty&field6=Empty&field7=Empty&field8=Empty')
 			diz1 = json.dumps(response.json(), indent = 4)
@@ -58,41 +56,37 @@ class OpenWebService():
 			return json.dumps(new_device, indent = 4)
 
 	def PUT(self, **params):
-		# il put mi gestisce tutte le modifiche
+		# il put mi gestisce tutte le richieste di put e delete che vengono indirizzate catalog
 		my_string = cherrypy.request.body.read()
 		to_update = json.loads(my_string) #dictionary
 		print(to_update)
-
 		#ROOM'S updates
-		if to_update["command"] == "update_api_key_read":
+		if to_update["command"] == "update_api_key_read": # update api key read
 			body = to_update["body"]
 			roomID = body["roomID"]
 			del body["roomID"]
-			#print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/TS_get', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)		
-		elif to_update["command"] == "update_api_key_write":
+		elif to_update["command"] == "update_api_key_write": # update api key write
 			body = to_update["body"]
 			roomID = body["roomID"]
 			del body["roomID"]
-			#print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/TS_post', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)			
-		elif to_update["command"] == "update_channel_id":
+		elif to_update["command"] == "update_channel_id": # update channel id
 			body = to_update["body"]
 			roomID = body["roomID"]
 			del body["roomID"]
-			#print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/TS_channel', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)			
-		elif to_update["command"] == "update_product_type":
+		elif to_update["command"] == "update_product_type": # update product type
 			body = to_update["body"]
 			roomID = body["roomID"]
 			del body["roomID"]
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/change_product_type', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "change_ranges":
+		elif to_update["command"]== "change_ranges": # update api key read
 			body = to_update["body"]
 			roomID = body["roomID"]
 			del body["roomID"]
@@ -100,7 +94,7 @@ class OpenWebService():
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/change_ranges', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
 		# DEVICES PUT	
-		elif  to_update["command"]== "update_name":
+		elif  to_update["command"]== "update_name": # update name
 			body = to_update["body"]
 			roomID = body["roomID"]
 			deviceID = body["deviceID"]
@@ -109,7 +103,7 @@ class OpenWebService():
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/{deviceID}/update_name', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "change_meas_type":
+		elif to_update["command"]== "change_meas_type": # update measure type
 			body = to_update["body"]
 			roomID = body["roomID"]
 			deviceID = body["deviceID"]
@@ -118,7 +112,7 @@ class OpenWebService():
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/{deviceID}/change_meas_type', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "add_service_details":
+		elif to_update["command"]== "add_service_details": # update service details
 			body = to_update["body"]
 			roomID = body["roomID"]
 			deviceID = body["deviceID"]
@@ -127,7 +121,7 @@ class OpenWebService():
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/{deviceID}/add_service_details', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "change_topic":
+		elif to_update["command"]== "change_topic": # update topic
 			body = to_update["body"]
 			roomID = body["roomID"]
 			deviceID = body["deviceID"]
@@ -136,7 +130,7 @@ class OpenWebService():
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/{deviceID}/change_topic', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "change_field":
+		elif to_update["command"]== "change_field": # update fields
 			body = to_update["body"]
 			roomID = body["roomID"]
 			deviceID = body["deviceID"]
@@ -146,34 +140,35 @@ class OpenWebService():
 			r = requests.put(self.url_catalog + f'/catalog/{roomID}/{deviceID}/change_field', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
 		# USER PUT
-		elif to_update["command"]== "add_assigned_rooms":
+		elif to_update["command"]== "add_assigned_rooms": # add assigned room/rooms
 			body = to_update["body"]
 			userID = body["userID"]
 			del body["userID"]
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{userID}/add_assigned_rooms', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "delete_assigned_rooms":
+		elif to_update["command"]== "delete_assigned_rooms": # delete assigned room/rooms
 			body = to_update["body"]
 			userID = body["userID"]
 			del body["userID"]
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{userID}/delete_assigned_rooms', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "change_role":
+		elif to_update["command"]== "change_role": # change user role
 			body = to_update["body"]
 			userID = body["olduserID"]
 			del body["olduserID"]
 			print(json.dumps(body, indent = 4))
 			r = requests.put(self.url_catalog + f'/catalog/{userID}/change_role', data=json.dumps(body, indent=4))
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "delete_device":
+		# DELETE
+		elif to_update["command"]== "delete_device": # delete a device
 			body = to_update["body"]
 			roomID = body["roomID"]
 			deviceID = body["deviceID"]
 			r = requests.delete(self.url_catalog + f'/catalog/{roomID}/{deviceID}/delete')
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "delete_room":
+		elif to_update["command"]== "delete_room": # delete a room
 			body = to_update["body"]
 			roomID = body["roomID"]
 			#mi serve il channel_id della stanza per eliminare il canale
@@ -182,10 +177,9 @@ class OpenWebService():
 			diz = json.loads(diz1) 
 			channelID = diz["ThingSpeak"]["channelID"]
 			r = requests.delete(self.url_catalog + f'/catalog/{roomID}/delete')
-			#delete also thingspeak field
 			response = requests.delete(self.url_delete_channel_thingspeak + f'/{channelID}' f'?api_key={self.api_key}')
 			return json.dumps(to_update, indent = 4)
-		elif to_update["command"]== "delete_user":
+		elif to_update["command"]== "delete_user": # delete user
 			body = to_update["body"]
 			userID = body["userID"]
 			r = requests.delete(self.url_catalog + f'/catalog/{userID}/delete')
@@ -195,6 +189,9 @@ class OpenWebService():
 
 
 if __name__ == '__main__':
+	settings = json.load(open('Settings.json'))
+	url_catalog = settings["catalogURL"] # prendo dai setting l'url del catalog
+	api_key = settings["api_key_thingspeak"] # prendo dai setting la chiave per creare canali thingspeak
 	conf={
 		'/':{
 				'request.dispatch':cherrypy.dispatch.MethodDispatcher(),
@@ -209,7 +206,7 @@ if __name__ == '__main__':
 		 'tools.staticdir.dir':'./js'
 		 },
 	}		
-	cherrypy.tree.mount(OpenWebService(),'/',conf)
+	cherrypy.tree.mount(OpenWebService(url_catalog, api_key),'/',conf)
 	cherrypy.config.update({'server.socket_port': 8090})
 	cherrypy.engine.start()
 	cherrypy.engine.block()
--- PROGETTO/Warehouse_Manager/js/myform.js
+++ PROGETTO/Warehouse_Manager/js/myform.js
@@ -1,4 +1,6 @@
-// POST
+/* tutte le funzioni qui presenti vengono richiamate dalla form al click dei vari button e servono per inviare delle richieste
+POST e PUT al Register che a sua volta legge il body di queste richieste che contiene un "command" e in base a questo
+effettua le giuste richieste POST, PUT, DELETE al catalog */
 
 // FUNZIONE PER L'INSERIMENTO DEL DEVICE
 function sendDevicesPOSTrequest() {
@@ -11,7 +13,8 @@ function sendDevicesPOSTrequest() {
   deviceName = $("#deviceName").val();
   measureType = $("#measureType").val();
   field = $("#field").val();
-
+  field2 = $("#field2").val();
+  // primo service della form
   service1 = $("#service1").val();
   serviceip1 = $("#serviceIP").val();
   service1_topic1 = $("#topic1").val();
@@ -28,7 +31,7 @@ function sendDevicesPOSTrequest() {
       k = k+1;
     }
   }
-  
+  // secondo service della form
   service2 = $("#service2_http").val();
   serviceip2 = $("#serviceIP2").val();
   service2_topic1 = $("#topic11").val();
@@ -46,14 +49,14 @@ function sendDevicesPOSTrequest() {
     }
   }
 
-  // anche i services bisogna controllare, se il secondo è NULL allora
+  // anche i services bisogna controllare, se il secondo è "none" allora
   // vuol dire che ce n'è solo 1 e bisogna eliminare i campi nulli della form
 
   services_array = [service1, service2];
   services = [];
   services_details = [];
 
-  if (services_array[1] == 'none'){
+  if (services_array[1] == '-'){
     services = services_array[0];
     services_details = [{
       serviceType: service1,
@@ -61,7 +64,7 @@ function sendDevicesPOSTrequest() {
       topic: topics1_array
     }]
   };
-
+  // tutte le combinazioni dei service che possono essere selezionati e la costruzione dei campi del body
   if (services_array[1] == 'REST' && services_array[0] == 'MQTT'){
     services = services_array;
     services_details = [{
@@ -100,7 +103,13 @@ function sendDevicesPOSTrequest() {
       topic: topics2_array
     }]
   };
-  
+  // necessario nel caso in cui come nel DHT11 è necessario avere più fields
+  fields = [];
+  if (field2 == 'none') {
+    fields = [field];
+  } else {
+    fields = [field, field2];
+  }
   // il body è quello che verrà poi inviato nel catalog, mentre il command andrà a definire l'ulr
   body = {
     roomID : roomID,
@@ -109,9 +118,9 @@ function sendDevicesPOSTrequest() {
     measureType: measureType,
     availableServices : services,
     servicesDetails: services_details,
-    field: field,
+    field: fields
     }; 
-
+    // richiesta post da effettuare al register
   $.ajax({
     type: "POST",
     url: "/",
@@ -123,6 +132,7 @@ function sendDevicesPOSTrequest() {
 // FUNZIONE PER L'INSERIMENTO DELLA STANZA
 function sendRoomsPOSTrequest() {
   event.preventDefault();
+  // si pendono i campi dalla form
   roomID = $("#room_ID").val();
   product_type = $("#product_type").val();
   temp_min = parseInt($("#temp_min").val());
@@ -131,7 +141,7 @@ function sendRoomsPOSTrequest() {
   hum_max = parseInt($("#hum_max").val());
   smoke_min = parseInt($("#smoke_min").val());
   smoke_max = parseInt($("#smoke_max").val());
-
+  // costruzione del body da inviare
   body = {
     roomID : roomID,
     devicesList: [],
@@ -139,7 +149,7 @@ function sendRoomsPOSTrequest() {
     product_type: product_type,
     ThingSpeak:{"channelID": "", "api_key_read": "", "api_key_write": ""}
     };
-
+    // richiesta POST al Register
     $.ajax({
       type: "POST",
       url: "/",
@@ -155,11 +165,11 @@ function sendUsersPOSTrequest(){
   roomID = String($("#roomIDs").val());
   userID = $("#userID").val(); 
   roomIDs = [];
-
+  // se sono più stanze le separo con il trattino come indicato sulla form
   if (roomID.search("-") != -1){
     roomIDs = roomID.split("-");    
   }else{roomIDs = [roomID]}
-
+  // costruzione del body
   body = {
     roomIDs : roomIDs,
     userID: userID,
@@ -175,18 +185,19 @@ function sendUsersPOSTrequest(){
     });
 
 }
-
-// // ROOM UPDATE (PUT)
-
-/* api_key_update {“api_key_read”: ZVBAO2QDON8B19X0} catalog/ID_stanza/TS_get */
+// ************************ROOM UPDATE (PUT)************************************
+/* api_key_update {“api_key_read”: ZVBAO2QDON8B19X0} catalog/ID_stanza/TS_get -> MODIFICA API KEY READ */ 
 function sendRoomApiKeyReadPUTrequest(){
+  // leggo i campi dalla form
   roomID = $("#roomID3").val();
   api_key_read = $("#api_key_r").val();  
   command = 'update_api_key_read';
+  // costruzione del body
   body = {
     roomID : roomID,
     api_key_read : api_key_read
   };
+  // richiesta PUT alla form
   $.ajax({
     type: "PUT",
     url: "/",
@@ -196,15 +207,18 @@ function sendRoomApiKeyReadPUTrequest(){
   });
 
 }
-// api_key_write {“api_key_write”: S6ULMDXZPCVFBR0H}  catalog/ID_stanza/TS_post
+// api_key_write {“api_key_write”: S6ULMDXZPCVFBR0H}  catalog/ID_stanza/TS_post -> MODIFICA API KEY WRITE
 function sendRoomApiKeyWritePUTrequest(){
+  // leggo i campi dalla form
   roomID = $("#roomID4").val();
   api_key_write = $("#api_key_w").val();  
   command = 'update_api_key_write';
+  // costruisco il body
   body = {
     roomID : roomID,
     api_key_write : api_key_write
   };
+  // put request
   $.ajax({
     type: "PUT",
     url: "/",
@@ -213,17 +227,18 @@ function sendRoomApiKeyWritePUTrequest(){
     dataType: "json",
   });
 }
-/*channel id {“channelID”: 134252} catalog/ID_stanza/TS_channel*/
+/*channel id {“channelID”: 134252} catalog/ID_stanza/TS_channel* -> MODIFICA CHANNEL ID */
 function sendRoomChannelPUTrequest(){
-
+  // leggo i campi dalla form
   roomID = $("#roomID_2").val();
   channelID= $("#chann_id").val();  
   command = "update_channel_id";
+  // costruisco il body
   body = {
     roomID : roomID,
     channelID : channelID
   };
-
+  // richiesta PUT al register
   $.ajax({
     type: "PUT",
     url: "/",
@@ -232,15 +247,18 @@ function sendRoomChannelPUTrequest(){
     dataType: "json",
   });  
 }
-// update product type {"product_type": "00"} catalog/ID_stanza/change_product_type
+// update product type {"product_type": "00"} catalog/ID_stanza/change_product_type -> MODIFICA DEL PRODUCT TYPE
 function sendRoomProductPUTrequest(){
+  // leggo i campi dalla form
   roomID1 = $("#roomID_1").val();
   product_type = $("#prod_type").val();  
   command = 'update_product_type';
+  // costruisco il body
   body = {
     roomID : roomID1,
     product_type : product_type
   };
+  // richiesta PUT 
   $.ajax({
     type: "PUT",
     url: "/",
@@ -249,16 +267,19 @@ function sendRoomProductPUTrequest(){
     dataType: "json",
   }); 
 }
-// update temperature ranges
+// update temperature ranges {"ranges": {"Temperature":[min, max]}} -> MODIFICA DEI RANGES DI TEMPERATURA
 function sendRoomTemperaturePUTrequest() {
+  // leggo i campi dalla form
   roomID1 = $("#roomID5").val();
   temp_min = parseInt($("#temp_min_1").val());  
   temp_max = parseInt($("#temp_max_1").val()); 
   command = 'change_ranges';
+  // costruisco il body
   body = {
     roomID : roomID1,
     ranges:{Temperature : [temp_min, temp_max]}
   };
+  // invio la richiesta
   $.ajax({
     type: "PUT",
     url: "/",
@@ -267,15 +288,19 @@ function sendRoomTemperaturePUTrequest() {
     dataType: "json",
   }); 
 }
+// update Humidity ranges {"ranges": {"Humidity":[min, max]}} -> MODIFICA DEI RANGES DI umidità
 function sendRoomHumidityPUTrequest() {
+  // leggo i campi della form
   roomID1 = $("#roomID6").val();
   hum_min = parseInt($("#hum_min_1").val());  
   hum_max = parseInt($("#hum_max_1").val()); 
   command = 'change_ranges';
+  // costruisco il body
   body = {
     roomID : roomID1,
     ranges:{Humidity : [hum_min, hum_max]}
   };
+  // richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -284,15 +309,19 @@ function sendRoomHumidityPUTrequest() {
     dataType: "json",
   }); 
 }
+// update Smoke ranges ranges {"ranges": {"Smoke":[min, max]}} -> MODIFICA DEI RANGES DI smoke
 function sendRoomSmokePUTrequest() {
+  // leggo i campi dalla form
   roomID1 = $("#roomID7").val();
   smoke_min = parseInt($("#smoke_min_1").val());  
   smoke_max = parseInt($("#smoke_max_1").val()); 
   command = 'change_ranges';
+  // costruisco il body
   body = {
     roomID : roomID1,
     ranges:{Smoke : [smoke_min, smoke_max]}
   };
+  // richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -302,19 +331,21 @@ function sendRoomSmokePUTrequest() {
   }); 
 }
 
-// devices updates
-
+// ***************************devices updates*******************************************
 // change name {"deviceName":"Pippo"} catalog/ID_stanza/ID_device/update_name
 function sendDeviceNamePUTrequest(){
+  // leggo i campi dalla form
   room = $("#room").val();
   device_id = $("#device_id").val();  
   device_name = $("#device_name").val(); 
   command = 'update_name';
+  // costruisco il body
   body = {
     roomID : room,
     deviceID: device_id,
     deviceName : device_name
   };
+  // faccio una richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -325,15 +356,18 @@ function sendDeviceNamePUTrequest(){
 }
 // change measure Type  catalog/ID_stanza/ID_device/change_meas_type ->{"measureType":"Celsius"}
 function sendDeviceMeasurePUTrequest(){
+  // leggo i campi dalla form
   room = $("#roomD").val();
   device_id = $("#device_idD").val();  
   meas_type = $("#meas_typeD").val(); 
   command = 'change_meas_type';
+  // costruisco il body
   body = {
     roomID : room,
     deviceID: device_id,
     measureType : meas_type
   };
+  // richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -357,13 +391,12 @@ function sendDeviceServicePUTrequest(){
     //leggo tutti gli input dalla form html com l'id/name che hanno
     roomID = $("#roomid").val();
     deviceID = $("#device_Id").val();
-    
+    // primo service della form
     service1 = $("#service_1").val();
     serviceip1 = $("#service__IP").val();
     service1_topic1 = $("#topic111").val();
     service1_topic2 = $("#topic222").val();
-    service1_topic3 = $("#topic333").val();
-  
+    service1_topic3 = $("#topic333").val();  
    // se i topics non sono vuoti allora si crea la lista definitiva dei topics
     topics_service1 = [service1_topic1, service1_topic2,service1_topic3];
     topics1_array = [];
@@ -374,13 +407,12 @@ function sendDeviceServicePUTrequest(){
         k = k+1;
       }
     }
-    
+    // secondo service della form
     service2 = $("#service_2").val();
     serviceip2 = $("#service__IP2").val();
     service2_topic1 = $("#topic_11").val();
     service2_topic2 = $("#topic_22").val();
-    service2_topic3 = $("#topic_33").val();
-  
+    service2_topic3 = $("#topic_33").val();  
     // se i topics non sono vuoti allora si crea la lista definitiva dei topics
     topics_service2 = [service2_topic1, service2_topic2,service2_topic3];
     topics2_array = [];
@@ -390,15 +422,12 @@ function sendDeviceServicePUTrequest(){
         topics2_array[k] = topics_service2[i];
         k = k+1;
       }
-    }
-  
-    // anche i services bisogna controllare, se il secondo è NULL allora
-    // vuol dire che ce n'è solo 1 e bisogna eliminare i campi nulli della form
-  
+    }  
+    // anche i services bisogna controllare, se il secondo è none allora
+    // vuol dire che ce n'è solo 1 e bisogna eliminare i campi nulli della form  
     services_array = [service1, service2];
     services = [];
-    services_details = [];
-  
+    services_details = [];  
     if (services_array[1] == 'none'){
       services = services_array[0];
       services_details = [{
@@ -407,7 +436,7 @@ function sendDeviceServicePUTrequest(){
         topic: topics1_array
       }]
     };
-  
+    // tutte le possibile scelte dalla form per costruire bene i campi del body
     if (services_array[1] == 'REST' && services_array[0] == 'MQTT'){
       services = services_array;
       services_details = [{
@@ -447,14 +476,14 @@ function sendDeviceServicePUTrequest(){
       }]
     };
     
-    // il body è quello che verrà poi inviato nel catalog, mentre il command andrà a definire l'ulr
+    // costruisco il body che è quello che verrà poi inviato nel catalog, mentre il command andrà a definire l'ulr
     body = {
       roomID : roomID,
       deviceID: deviceID,
       availableServices : services,
       servicesDetails: services_details
       }; 
-  
+    // richiesta PUT al Register
     $.ajax({
       type: "PUT",
       url: "/",
@@ -464,24 +493,25 @@ function sendDeviceServicePUTrequest(){
     });
 
 }
-
 // change topics catalog/ID_stanza/ID_device/change_topic {"topic": ["MySmartThingy/1/temp","MySmartThingy/1/hum"]}
 function sendDeviceTopicPUTrequest(){
+    // leggo i campi dalla form
     room = $("#room__D").val();
     device_id = $("#device__idD").val();  
     topic = $("#topic_D").val(); 
     command = 'change_topic';
     topics = [];
-    
+    // se sono più di 1 creo una lista separando i campi dai '-'
     if (topic.search("-") != -1){
       topics = topic.split("-");    
     }else{topics = [topic]}
-
+    // creo il body
     body = {
       roomID : room,
       deviceID: device_id,
       topic : topics
     };
+    // richiesta PUT al Register
     $.ajax({
       type: "PUT",
       url: "/",
@@ -492,16 +522,26 @@ function sendDeviceTopicPUTrequest(){
 }
 // change field catalog/ID_stanza/ID_Device/change_field ->  {"field": 'field1'}
 function sendDeviceFieldPUTrequest(){
+  // leggo i campi della form
   room = $("#roomID10").val();
   device_id = $("#device_id123").val();  
   field = $("#field_123").val(); 
+  field2 = $("#field_2").val(); 
   command = 'change_field';
-
+  // necessario if nel caso di più fields come per DHT11
+  fields = [];
+  if (field2 == 'none') {
+    fields = [field];
+  } else {
+    fields = [field, field2];
+  }
+  // costruisco il body
   body = {
     roomID : room,
     deviceID: device_id,
-    field: field
+    field: fields
   };
+  // faccio la richiesta PUT 
   $.ajax({
     type: "PUT",
     url: "/",
@@ -512,22 +552,24 @@ function sendDeviceFieldPUTrequest(){
 
 }
 
-//PUT USERS
-//add a room /catalog/User_ID/add_asigned_rooms 
+// *******************************PUT USERS********************************************
+// add a room /catalog/User_ID/add_asigned_rooms 
 function sendUserRoomsAddPUTrequest(){
+  // leggo i campi della form
   userID = $("#user_id").val();
   roomID = $("#rooms").val();
   command = 'add_assigned_rooms';
-
+  // se ci sono più stanze le separo dal '-'
   rooms = [];    
     if (roomID.search("-") != -1){
       rooms = roomID.split("-");    
     }else{rooms = [roomID]}
-
+  // costruisco il body
   body = {
     roomIDs : rooms,
     userID: userID
   };
+  // faccio la richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -538,19 +580,21 @@ function sendUserRoomsAddPUTrequest(){
 }
 //remove a room /catalog/User_ID/delete_asigned_rooms 
 function sendUserRoomsDeletePUTrequest(){
+  // leggo i campi dalla form
   userID = $("#user_id").val();
   roomID = $("#rooms").val();
   command = 'delete_assigned_rooms';
-
+  // se sono più di una si separano dal '-'
   rooms = [];    
     if (roomID.search("-") != -1){
       rooms = roomID.split("-");    
     }else{rooms = [roomID]}
-
+  // costruzione del body
   body = {
     roomIDs : rooms,
     userID: userID
   };
+  // richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -559,17 +603,18 @@ function sendUserRoomsDeletePUTrequest(){
     dataType: "json",
   });
 }
-
 //change role {"userID": "nuovo_userID"}
 function sendUserRolePUTrequest(){
+  // leggo i campi dalla form
   olduserID = $("#old_user_id").val();
   userID = $("#new_user_id").val();
   command = 'change_role';
-
+  // costruzione del body
   body = {
     olduserID : olduserID,
     userID: userID
   };
+  // richiesta PUT 
   $.ajax({
     type: "PUT",
     url: "/",
@@ -578,17 +623,19 @@ function sendUserRolePUTrequest(){
     dataType: "json",
   });
 }
-
-//DELETE FUNCTIONS
+//*************************DELETE FUNCTIONS**************************
+// delete devices
 function sendDeviceDeleteRequest(){
+  // leggo i campi dalla form
   roomID = $("#id_room").val();
   deviceID = $("#id_device").val();
   command = 'delete_device';
-
+  // costruisco il body
   body = {
     roomID : roomID,
     deviceID: deviceID
   };
+  // richiesta PUT
   $.ajax({
     type: "PUT",
     url: "/",
@@ -597,13 +644,15 @@ function sendDeviceDeleteRequest(){
     dataType: "json",
   });
 }
+// delete users
 function sendUserDeleteRequest(){
   userID = $("#id_user").val();
   command = 'delete_user';
-
+// leggo il body
   body = {
     userID : userID
   };
+// invio richiesta
   $.ajax({
     type: "PUT",
     url: "/",
@@ -613,13 +662,15 @@ function sendUserDeleteRequest(){
   });
   
 }
+// delete rooms
 function sendRoomDeleteRequest(){
   roomID = $("#id1_room").val();
   command = 'delete_room';
-
+  // costruisco il body
   body = {
     roomID : roomID
   };
+  // invio richiesta
   $.ajax({
     type: "PUT",
     url: "/",
@@ -628,9 +679,7 @@ function sendRoomDeleteRequest(){
     dataType: "json",
   });
 }
-
 function sendRoomPUTrequest(){
 }
-
 function sendDevicePUTrequest(){
 }
