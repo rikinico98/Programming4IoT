26bf2043faa5db09d01ade8527bd9e7046f320bc
EsCODEbar98 <62204561+EsCODEbar98@users.noreply.github.com>
2021-06-10 21:14:09 +0200

    HUGE CHANGES

--- Lab_4/Riccardo/prova.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import time
-timeout=time.time()+120
-while time.time()< timeout :
-    shape, scale = 2., 2.
-    a = 10*np.random.gamma(shape, scale, 1)+30
-    print(a)
-    time.sleep(2)
